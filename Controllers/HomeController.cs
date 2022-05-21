583af3a8aa2977f2b36899281849f682177e2f28
ABR-Developer <bsef19m005@pucit.edu.pk>
2022-05-21 13:18:25 +0500

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Citizen_Portal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View("index");
+        }
+
+        public ViewResult AboutUs()
+        {
+            return View("AboutUs");
+        }
+
+        [HttpGet]
+        public ViewResult SignUp()
+        {
+            return View("SignUp");
+        }
+        public ViewResult Register()
+        {
+            return View("Register");
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace Citizen_Portal.Models
+{
+    public class User
+    {
+        public string Name {    get;    set;    }
+        public string CNIC {    get;    set;    }
+        public string Email {    get;    set;    }
+        public string Password {    get;    set;    }
+        public string MobileNumber {    get;    set;    }
+        public string DOB {    get;    set;    }
+        public string Gender {    get;    set;    }
+        public string Province {    get;    set;    }
+        public string District {    get;    set;    }
+        public string Tehsil {    get;    set;    }
+        public string Address {    get;    set;    }
+        public string Status {    get;    set;    }
+    }
+}
